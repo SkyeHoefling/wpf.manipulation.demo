8c54134d609bf552baca7b0dde16dc87ff6b5b5a
Andrew Hoefling <andrew@hoeflingsoftware.com>
2016-12-12 04:25:02 +0000

    created touch control and created manipulation stubs

---
+++ WPF.ManipulationDemo/Controls/TouchControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPF.ManipulationDemo.Controls
+{
+    /// <summary>
+    /// Interaction logic for TouchControl.xaml
+    /// </summary>
+    public partial class TouchControl : UserControl
+    {
+        public TouchControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnManipulationStarting(ManipulationStartingEventArgs e)
+        {
+            base.OnManipulationStarting(e);
+        }
+
+        protected override void OnManipulationDelta(ManipulationDeltaEventArgs e)
+        {
+            base.OnManipulationDelta(e);
+        }
+
+        protected override void OnManipulationCompleted(ManipulationCompletedEventArgs e)
+        {
+            base.OnManipulationCompleted(e);
+        }
+    }
+}
